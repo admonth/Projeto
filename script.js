33cd221b853063b593bb20f37610cf07aa6ab269
admonth <admonithiago@gmail.com>
2024-06-28 20:02:36 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function toggleMode() {
+    const html = document.documentElement
+    
+   /*if(html.classList.contains('light')) {
+        html.classList.remove('light')
+    } else {
+        html.classList.add('light')
+    }*/
+
+    html.classList.toggle('light') /*É a mesma coisa do comentário de cima, só que é uma função simplificada, alguém criou e ta no js, é mais limpo do que deixar esse if e else, mas na prática fazem a mesma coisa*/
+
+    //pegar a tag img
+    const img = document.querySelector("#profile img")
+
+    // substituir a imagem
+    if(html.classList.contains('light')) {
+    //se tiver light mode, adicionar a imagem light
+    img.setAttribute('src', "./assets/whitecat.png")
+    img.setAttribute('alt', 'Foto de avatar do perfil de gato branco')
+    } else {
+        //se tiver sem light mode, manter a imagem normal
+        img.setAttribute('src', "./assets/blackcat.png")
+        img.setAttribute('alt', 'Foto de avatar do perfil de gato preto')
+    }
+}
